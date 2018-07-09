17c300ad416cac2fc4c4981d84e436061a8d156a
Chris McKeown <Chris.McKeown@bedegaming.com>
2018-07-09 15:00:18 +0100

    Don't manage config if we're uninstalling it

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,25 +1,28 @@
 # Configures NXLog by building a config file
 class nxlog::config (
-  $conf_dir   = $::nxlog::conf_dir,
-  $conf_file  = $::nxlog::conf_file,
-  $nxlog_root = $::nxlog::nxlog_root,) {
-  concat { "${conf_dir}/${conf_file}":
-    ensure         => present,
-    ensure_newline => true,
-  }
+  $conf_dir       = $::nxlog::conf_dir,
+  $conf_file      = $::nxlog::conf_file,
+  $nxlog_root     = $::nxlog::nxlog_root,
+  $ensure_setting = $::nxlog::ensure_setting, ) {
 
-  concat::fragment { 'conf_header':
-    target  => "${conf_dir}/${conf_file}",
-    content => template('nxlog/header.erb'),
-    order   => '01',
-  }
+  unless ($ensure_setting =~ /absent/) {
+    concat { "${conf_dir}/${conf_file}":
+      ensure         => present,
+      ensure_newline => true,
+    }
 
-  # Ensure there is a blank line at the end of the file
-  concat::fragment { 'conf_footer':
-    target  => "${conf_dir}/${conf_file}",
-    content => "\n",
-    order   => '99',
-  }
+    concat::fragment { 'conf_header':
+      target  => "${conf_dir}/${conf_file}",
+      content => template('nxlog/header.erb'),
+      order   => '01',
+    }
 
+    # Ensure there is a blank line at the end of the file
+    concat::fragment { 'conf_footer':
+      target  => "${conf_dir}/${conf_file}",
+      content => "\n",
+      order   => '99',
+    }
+  }
 }
 
